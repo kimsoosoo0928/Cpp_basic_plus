8976167c56e73bc0fc5a2a9d1248ca9cca1f502f
kimsoosoo0928 <kimsoosoo0928@gmail.com>
2022-02-11 09:11:21 +0900

    test

---
+++ 02_3/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+
+	int carrots;
+
+	cout << "당근을  몇 개나 가지고 있니?" << endl;
+	cin >> carrots;
+	cout << "여기 두 개가 더 있다.";
+	carrots = carrots + 2;
+
+	cout << "이제 당근을 모두 " << carrots << "개이다." << endl;
+	return 0;
+}
